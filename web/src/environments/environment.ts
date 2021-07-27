0faba06450b73799262a4d74a79304330b35c7c7
Neuber Sousa <38701397+neuberfran@users.noreply.github.com>
2021-07-27 01:13:32 -0300

    Update environment.ts

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -3,12 +3,12 @@ export const environment = {
   production: true,
  
   firebase: {
-    apiKey: "AIzaSyChfxrbgd5OEJlgy7aw8D7pvlaHsPM1hlI",
+    apiKey: "fadsfadsfadsfadsfasd",
     authDomain: "casaminha-2e0ca.firebaseapp.com",
     databaseURL: "https://casaminha-2e0ca.firebaseio.com",
     projectId: "casaminha-2e0ca",
     storageBucket: "casaminha-2e0ca.appspot.com",
-    messagingSenderId: "567617726062"
+    messagingSenderId: "999999999999999"
   },
-  clientId: "567617726062-9r9upj4tip1nfdupm3iin2ld6t7coa0t.apps.googleusercontent.com"
+  clientId: "99999999999999999999999999999999999.apps.googleusercontent.com"
 };
