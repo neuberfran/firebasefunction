98b3d22093f679f5c1a6e2f92523d031f78564d8
Neuber Sousa <38701397+neuberfran@users.noreply.github.com>
2023-07-22 20:53:25 -0300

    Update environment.prod.ts

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,14 +1,2 @@
 
-export const environment = {
-  production: true,
- 
-  firebase: {
-    apiKey: "fasdfadsfasdfasdfadsfasdfdasfdas",
-    authDomain: "casaminha-2e0ca.firebaseapp.com",
-    databaseURL: "https://casaminha-2e0ca.firebaseio.com",
-    projectId: "casaminha-2e0ca",
-    storageBucket: "casaminha-2e0ca.appspot.com",
-    messagingSenderId: "9999999999999999"
-  },
-  clientId: "999999999999999999999999999999996t7coa0t.apps.googleusercontent.com"
-};
+sss
